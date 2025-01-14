8deb69846de75e08e5f439cc8a26284dc7dfcc20
aleezaa-ada <aleeza.ahmed@ada.ac.uk>
2025-01-14 15:16:17 +0000

    primer 2 completed

--- primer2/primer2.js
+++ primer2/primer2.js
@@ -1,26 +1,56 @@
+const suits = ['Hearts', 'Diamonds', 'Clubs', 'Spades'];
+const values = ['Ace', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King'];
 
-// TODO: Write a function shuffleAndDeal(numPlayers, cardsPerPlayer, numDecks = 1)
-// that simulates shuffling and dealing a deck of cards.
+const createDeck = (numDecks = 1) => {
+    const deck = [];
+    for (let i = 0; i < numDecks; i++) {
+        suits.forEach(suit => {
+            values.forEach(value => {
+                deck.push({ value, suit });
+            });
+        });
+    }
+    return deck;
+};
 
-// TODO: Create a standard 52-card deck (or 104 if numDecks is 2).
+// Fisher-yates shuffle method with O(N) time and requires O(1) additional space. 
+const shuffleDeck = (deck) => {
+    for (let i = deck.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [deck[i], deck[j]] = [deck[j], deck[i]];
+    }
+    return deck;
+};
 
-// TODO: Implement input validation to handle invalid inputs:
-//       -  numPlayers should be a positive integer.
-//       -  cardsPerPlayer should be a positive integer.
-//       -  Throw an error if the requested cards exceed the deck size.
+const stringFormatter = (hands) => {
+    return hands.map(hand => hand.map(card => `${card.value} of ${card.suit}`));
+}
 
-// TODO: Shuffle the deck using a suitable algorithm
-//       -  Consider time complexity and randomness.
+const isValidInteger = (numPlayers, cardsPerPlayer, totalCards) => {
+    if (numPlayers <= 0) {
+        throw new Error("Number of players must be a positive integer.");
+    }
+    if (cardsPerPlayer <= 0) {
+        throw new Error("Cards per player must be a positive integer.");
+    }
+    if (numPlayers * cardsPerPlayer > totalCards) {
+        throw new Error("Not enough cards in the deck to deal the requested number of cards per player.");
+    }
+};
 
-// TODO: Deal cards to the specified number of players.
+const shuffleAndDeal = (numPlayers, cardsPerPlayer, numDecks = 1) => {
+    const deck = createDeck(numDecks);
 
-// TODO: Return the hands dealt as an array of arrays.
+    isValidInteger(numPlayers, cardsPerPlayer, deck.length);
 
-// TODO: Test the function with various inputs, including edge cases:
-//       -  Dealing the entire deck.
-//       -  Single player.
-//       -  Minimum cards per hand.
+    shuffleDeck(deck);
 
+    const hands = [];
+    for (let i = 0; i < numPlayers; i++) {
+        hands.push(deck.splice(0, cardsPerPlayer));
+    }
+
+    return stringFormatter(hands);
+};
 
 export default shuffleAndDeal;
-
